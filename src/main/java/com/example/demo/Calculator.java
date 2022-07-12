af560e59816c7d246120307aaca345c3770befda
Nancy967 <beg7520@gmail.com>
2022-07-12 18:38:25 +0800

    assert

--- src/main/java/com/example/demo/Calculator.java
+++ src/main/java/com/example/demo/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
     public int add(int x, int y){
         return x + y;
     }
+    public int divide(int x, int y){ return x / y;}
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
